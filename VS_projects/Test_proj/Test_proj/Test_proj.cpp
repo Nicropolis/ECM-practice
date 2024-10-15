d0e2365cebc35f5b9952c6bda0a2c1eb64016588
Nicropolis <mavorozhtsov@gmail.com>
2024-10-15 13:50:45 +0300

    Reverse right pyramid created in VS_projects/Test_proj

---
+++ VS_projects/Test_proj/Test_proj/Test_proj.cpp
@@ -0,0 +1,49 @@
+// Test_proj.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    //const int numLevelsC = 7;
+    int numLevels = 0;
+    std::cout << "Please input levels" << std::endl;
+    std::cin >> numLevels;
+    if (numLevels > 0)
+    {
+        for (int k = 1; k <= numLevels; ++k)
+        {
+            for (int i = 1; i < k; ++i)
+            {
+                std::cout << ' ';
+            }
+            for (int c = numLevels + 1; c > k; --c)
+            {
+                std::cout << '*';
+            }
+            if (k != numLevels)
+            {
+                std::cout << std::endl;
+            }
+        }
+    }
+    else
+    {
+        std::cout << "Wrong input" << std::endl;
+    }
+
+    //system("pause");
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
